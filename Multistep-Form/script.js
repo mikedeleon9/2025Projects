8b754c2dd737f7845fe13c11b5fad4edd2042a5f
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-28 16:43:32 -0500

    done for the day

---
+++ Multistep-Form/script.js
@@ -0,0 +1,26 @@
+const infoContainer = document.querySelector('.info-container')
+const stepNumbers = document.querySelectorAll('.number');
+let currentStep = 1;
+const blueColor = 'hsl(206, 94%, 87%)';
+
+function updateNumbers(){
+    stepNumbers.forEach(number => {
+        number.style.background = '';
+        number.style.color = '';
+
+        if(parseInt(number.textContent) === currentStep){
+            number.style.background = blueColor;
+            number.style.color = 'black'
+        } 
+    
+    })
+}
+updateNumbers();
+
+function nextStep(){
+    currentStep++;
+
+    updateNumbers();
+    
+};
+
