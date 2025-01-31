9c218984f9eb8311a9355de7369be7e013019cdc
mikedeleon9 <mikedeleon9@gmail.com>
2025-01-31 16:04:55 -0500

    need to add button to second component

--- react-multistep-form/src/components/SelectPlanComponent/Plans.jsx
+++ react-multistep-form/src/components/SelectPlanComponent/Plans.jsx
@@ -2,10 +2,12 @@
 
 export default function Plans(props){
     return(
-        <div className="border-1 border-(--navy)">
+        <div className="border-1 rounded-lg border-(--navy) p-3 flex flex-col items-start justify-between gap-8">
             <img src={props.image}></img>
-            <p>{props.option}</p>
-            <p>${props.price} /mo</p>
+            <div className="">
+            <p className="text-sm font-bold">{props.option}</p>
+            <p className="text-sm text-gray-400">${props.price} /mo</p>
+            </div>
         </div>
     )
 }
--- react-multistep-form/src/components/SelectPlanComponent/SelectPlan.jsx
+++ react-multistep-form/src/components/SelectPlanComponent/SelectPlan.jsx
@@ -5,7 +5,7 @@ import proImage from '../../assets/images/icon-pro.svg'
 
 export default function SelectPlan(){
     return(
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-4">
             <Plans image={arcadeImage} option="Arcade" price="9"/>
             <Plans image={advanceImage} option="Pro" price="12"/>
             <Plans image={proImage} option="Advanced" price="15"/>
