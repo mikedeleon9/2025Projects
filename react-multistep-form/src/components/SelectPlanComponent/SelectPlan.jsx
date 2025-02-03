749c486ecda853125f79079b80fa2366da0991bf
mikedeleon9 <mikedeleon9@gmail.com>
2025-02-02 21:59:58 -0500

    added switch between monthly and yearly

--- react-multistep-form/src/components/SelectPlanComponent/SelectPlan.jsx
+++ react-multistep-form/src/components/SelectPlanComponent/SelectPlan.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import Plans from "./Plans"
 import arcadeImage from '../../assets/images/icon-arcade.svg'
 import advanceImage from '../../assets/images/icon-advanced.svg'
@@ -5,17 +6,34 @@ import proImage from '../../assets/images/icon-pro.svg'
 
 
 export default function SelectPlan(){
+
+    const [selected, setSelected] = useState(null);
+
+    const handleChange = (checkbox) => {
+        setSelected(checkbox === selected ? null : checkbox);
+      };
+
+
     return(
         <div className="flex flex-col gap-8 ">
         <div className="grid grid-cols-3 gap-4 max-h-42">
-            <Plans image={arcadeImage} option="Arcade" price="9"/>
-            <Plans image={advanceImage} option="Pro" price="12"/>
-            <Plans image={proImage} option="Advanced" price="15"/>
+            <Plans image={arcadeImage} option="Arcade" price={selected === "monthly" ? "9" : "90"}/>
+            <Plans image={advanceImage} option="Pro" price={selected === "monthly" ? "12" : "120"}/>
+            <Plans image={proImage} option="Advanced" price={selected === "monthly" ? "15" : "150"}/>
         </div>
-        <div className="flex items-center justify-center bg-gray-100 p-1 rounded-lg gap-4">
+        <div className="flex items-center justify-center bg-gray-100 p-1 rounded-lg gap-8">
+            <div className="flex">
+            <input checked={selected === "monthly"} 
+           onChange={() => handleChange("monthly")}
+            type="checkbox"></input>
             <p>Monthly</p>
-           <input type="checkbox"></input>
+            </div>
+            <div className="flex">
+            <input checked={selected === "yearly"}
+            onChange={() => {handleChange("yearly")}}  
+            type="checkbox"></input>
             <p>Yearly</p>
+            </div>
         </div>
         </div>
     )
